7a0ea82599202347eb1d27be30533f3f967487ee
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-05-31 22:50:40 +0600

    conditional css added to country.jsx and country.css

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -12,16 +12,16 @@ const Country = ({country}) => {
     }
 
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'visited' : 'notvisited'}`}>
             <h3>{name.common}</h3>
             <img src={flags.png} />
             <p>Population: {population}</p>
             <p>Region: {region}</p>
             <p>Capital: {capital}</p>
             <p>Area: {area} km²</p>
-            <button onClick={handleVisited} className='visited'>{visited ? "Visited" : "Going"}</button>
+            <button onClick={handleVisited} className={`${visited ? 'visited-button' : 'notvisited-button'}`}>{visited ? "Visited" : "Going"}</button>
 
-            {visited ? <p className='visited-message'>You have visited this country!</p> : <p className='not-visited-message'>You have not visited this country yet.</p>}   
+            {visited ? <p>You have visited this country!</p> : <p>You have not visited this country yet.</p>}   
            
         </div>
     );
